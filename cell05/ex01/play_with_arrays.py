bb8ef83b960cd8fd081ef366007104ec8f6dc4e1
goui053336-rgb <goui053336@gmail.com>
2026-04-25 14:59:27 +0700

    Add files via upload

---
+++ cell05/ex01/play_with_arrays.py
@@ -0,0 +1,10 @@
+original = [2, 8, 9, 48, 8, 22, -12, 2]
+new = []
+
+i = 0
+while i < len(original):
+    new.append(original[i] + 2)
+    i += 1
+
+print("Original array:", original)
+print("New array:", new)
