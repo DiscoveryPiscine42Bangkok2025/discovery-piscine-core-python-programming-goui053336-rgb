a6e739bd2ce767e010d04ebad78319dd73be8b4e
goui053336-rgb <goui053336@gmail.com>
2026-04-25 11:29:21 +0700

    Add files via upload

---
+++ cell02/ex03/mult.py
@@ -0,0 +1,13 @@
+a = int(input())
+b = int(input())
+
+result = a * b
+
+if result > 0:
+    print("The result is positive.")
+elif result < 0:
+    print("The result is negative.")
+else:
+    print("The result is zero.")
+
+print(result)
